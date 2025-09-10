760edec9034e2e05fcb197b223dd7718b13a6d75
Nelson <nelsonavello1@gmail.com>
2025-09-10 01:58:57 -0300

    Incorporacion de Partials y Layouts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const layouts = require('express-ejs-layouts');
+
 
 const path = require('path');
 app.use(express.static(path.join(__dirname,"public")));
@@ -11,7 +13,11 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "src/views"));
 
+app.use(layouts);
+app.set('layout', 'layouts/layout');
+
 const mainRouter = require('./src/routes/main.router');
+const req = require('express/lib/request');
 app.use(mainRouter);
 
 app.use('/productos',require('./src/routes/productos.router'));
--- src/routes/main.router.js
+++ src/routes/main.router.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const controller = require('../controllers/main.controller');
 
 router.get('/', controller.index);  
-
+router.get('/privada', controller.private);
 
 module.exports = router;
